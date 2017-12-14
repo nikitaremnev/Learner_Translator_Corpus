8b3988b2b5d4afdf22e9ed9cd224e5ef7560eec6
Sokureo <pelmenium.sokurium@gmail.com>
2017-12-14 13:31:40 +0300

    wsgi edit

--- translator_corpus/wsgi.py
+++ translator_corpus/wsgi.py
@@ -12,6 +12,6 @@ import sys
 from django.core.wsgi import get_wsgi_application
 
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translation_corpus.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "translator_corpus.settings")
 
 application = get_wsgi_application()
